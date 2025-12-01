c3f56d8e9d3bb5acbc769527851125ed99080083
Xavier Balloy <686305+xballoy@users.noreply.github.com>
2025-12-01 13:30:05 -0500

    Day 01 (#1)
    * refactor: introduce Reindeer type to reduce number of parameters
    
    * refactor: use provider from constructor for Christmas date as it's not variable
    
    * chore: clean package.json
    
    * chore: journey for day 1

--- memories/2024/exercise/ts/day01/tests/santaCommunicator.spec.ts
+++ memories/2024/exercise/ts/day01/tests/santaCommunicator.spec.ts
@@ -1,34 +1,44 @@
 import {SantaCommunicator} from "../src/santaCommunicator";
 import {TestLogger} from "./doubles/testLogger";
+import {Reindeer} from "../src/reindeer";
 
-const SantaCommunicatorSpec = 'Dasher';
-const NORTH_POLE = 'North Pole';
 const numberOfDaysToRest = 2;
 const numberOfDayBeforeChristmas = 24;
+
+const dasher: Omit<Reindeer, 'numbersOfDaysForComingBack'> = {
+    reindeerName: 'Dasher',
+    currentLocation: 'North Pole',
+};
 
 describe('SantaCommunicator', () => {
     let communicator: SantaCommunicator;
     let logger: TestLogger;
 
     beforeEach(() => {
-        communicator = new SantaCommunicator(numberOfDaysToRest);
+        communicator = new SantaCommunicator(numberOfDaysToRest, {daysBeforeChristmasProvider: () => numberOfDayBeforeChristmas});
         logger = new TestLogger();
     });
 
     test('composeMessage', () => {
-        const message = communicator.composeMessage(SantaCommunicatorSpec, NORTH_POLE, 5, numberOfDayBeforeChristmas);
+        const message = communicator.composeMessage({...dasher, numbersOfDaysForComingBack: 5});
         expect(message).toEqual('Dear Dasher, please return from North Pole in 17 day(s) to be ready and rest before Christmas.');
     });
 
     test('shouldDetectOverdueReindeer', () => {
-        const overdue = communicator.isOverdue(SantaCommunicatorSpec, NORTH_POLE, numberOfDayBeforeChristmas, numberOfDayBeforeChristmas, logger);
+        const overdue = communicator.isOverdue({
+            ...dasher,
+            numbersOfDaysForComingBack: numberOfDayBeforeChristmas
+        }, logger);
 
         expect(overdue).toBeTruthy();
         expect(logger.getLog()).toEqual('Overdue for Dasher located North Pole.');
     });
 
     test('shouldReturnFalseWhenNoOverdue', () => {
-        const overdue = communicator.isOverdue(SantaCommunicatorSpec, NORTH_POLE, numberOfDayBeforeChristmas - numberOfDaysToRest - 1, numberOfDayBeforeChristmas, logger);
+        const overdue = communicator.isOverdue({
+            ...dasher,
+            numbersOfDaysForComingBack: numberOfDayBeforeChristmas - numberOfDaysToRest - 1
+        }, logger);
         expect(overdue).toBeFalsy();
     });
 });
